d92581145605aeb54a3d4f75643cf7a5169b50fb
Tim Turner <timdturner@gmail.com>
2018-04-22 17:10:30 -0400

    Update projects & about me page

--- src/pages/project-list/project-list.ts
+++ src/pages/project-list/project-list.ts
@@ -1,13 +1,44 @@
 export class ProjectList {
     projects: Project[] = [
         {
-            title: "Aspen Unified PIMS",
+            title: "Aspen Unified",
             image: "aspen",
             status: "Released",
             summary: "Web-based oil refinery optimization platform.",
             link: "https://www.aspentech.com/en/resources/video/planning-is-personal---introducing-unified-pims",
             linkTitle: "Visit Website",
+            role: "Senior Developer & UX Lead",
             github: null
+        },
+        {
+            title: "Massive Deeps",
+            image: "massive-deeps",
+            status: "Personal Brand",
+            summary: "Branding for my personal projects and homelab",
+            link: "https://www.figma.com/file/SmMmBJOq9TBgUHEigjVPh6p8/Massive-Deeps",
+            linkTitle: "View on Figma",
+            role: "Designer",
+            github: null
+        },
+        {
+            title: "Radarr",
+            image: "radarr",
+            status: "Released",
+            summary: "Open Source Movie PVR, used with Plex",
+            link: "http://radarr.video/",
+            linkTitle: "Visit Website",
+            role: "Lead Designer (UI & UX), Lead Frontend Developer",
+            github: "https://github.com/Radarr/Radarr",
+        },
+        {
+            title: "VR Meditations",
+            image: "vr-meditations",
+            status: "In Development",
+            summary: "Meditation scenes for WebVR",
+            link: "https://fedoranimus.github.io/vr-meditations/",
+            linkTitle: "Visit Website",
+            role: "Personal Project",
+            github: "https://github.com/fedoranimus/vr-meditations"
         },
         {
             title: "Today",
@@ -16,6 +47,7 @@ export class ProjectList {
             summary: "Chrome extension for completing Todoist tasks via the Pomodoro method.",
             link: "https://chrome.google.com/webstore/detail/today/nlaopmodobgckcknfffmkpcgfoilbeig",
             linkTitle: "Visit Website",
+            role: "Personal Project",
             github: "https://github.com/fedoranimus/today-app"
         },
         {
@@ -25,6 +57,7 @@ export class ProjectList {
             summary: "Web app to help people to find mentors. Targeted towards college educated individuals in STEM fields.",
             link: "https://becovalent.com",
             linkTitle: "Visit Website",
+            role: "Personal Project",
             github: "https://github.com/fedoranimus/Covalence"
         },
         {
@@ -33,6 +66,7 @@ export class ProjectList {
             status: "Internal",
             summary: "Gamified Timesheet tracking built for Modern Climate during Internship.",
             link: null,
+            role: "Lead Developer",
             linkTitle: "Case Study coming soon",
             github: null
         },
@@ -43,6 +77,7 @@ export class ProjectList {
             summary: "An early augmented reality game built while interning at Modern Climate. Nominated for a Webby award.",
             link: "http://www.modernclimate.com/work/scratch-pad-fever",
             linkTitle: "Visit Website",
+            role: "Developer",
             github: null
         }
     ];
@@ -60,4 +95,5 @@ interface Project {
     link: string | null;
     linkTitle: string;
     github: string | null;
+    role: string;
 }
